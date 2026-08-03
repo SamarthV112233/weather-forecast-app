ef9d4b52d355d75d8a10bb15f47c5bcba4abfb0c
bhadri <etechnopack@gmail.com>
2026-08-03 16:01:06 -0400

    Filtering raw weather data

--- backend.py
+++ backend.py
@@ -3,12 +3,19 @@ import requests
 API_KEY = "4320a22c738ae2ee1e3c58132bccb393"
 
 
-def get_data(place, forecast_days=None, condition=None):
+def get_data(place, forecast_days=None, kind=None):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Conditions":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
