8c56a2989b6704ad252afdc02a3750631b4265e3
bhadri <etechnopack@gmail.com>
2026-08-05 16:29:29 -0400

    Add conditions

--- main.py
+++ main.py
@@ -11,18 +11,32 @@ days = st.slider(
                  help="Select the number of forecasted days."
                 )
 option = st.selectbox(
-                      label="Select data tp view",
+                      label="Select data to view",
                       options=("Temperature", "Conditions")
                      )
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
+if place:
+    filtered_data = get_data(place, days)
 
-d, t = get_data(days)
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(
+                         x=dates,
+                         y=temperatures,
+                         labels={"x": "Date", "y": "Temperature (C)"}
+                        )
+        st.plotly_chart(figure)
 
-figure = px.line(
-                 x=d,
-                 y=t,
-                 labels={"x": "Date", "y": "Temperature (C)"}
-                )
-st.plotly_chart(figure)
+    if option == "Conditions":
+        images = {
+                  "Clear": "images/clear.png",
+                  "Clouds": "images/cloud.png",
+                  "Rain": "images/rain.png",
+                  "Snow": "images/snow.png"
+                 }
+        conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_path = [images[condition] for condition in conditions]
+
+        st.image(image_path, width=115)
